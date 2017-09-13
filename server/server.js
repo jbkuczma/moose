edefb8a6670b1cf95946cb2b0b84216a8a9e65eb
James <jbkuczma@buffalo.edu>
2017-09-13 18:03:25 -0400

    for #5 added more routes

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
 
-app.get('/', function(request, response) {
-    response.send('Hello');
+app.get('/login', function(request, response) {
+    response.send('login page');
 });
+
+app.get('/rooms', function(request, response) {
+    response.send('create or join a room page');
+});
+
+app.get('/room/:roomCode', function(request, response) {
+    respone.send('a room page')
+})
 
 app.listen(3000, function() {
     console.log('Moose started on port 3000. Go to localhost:3000');
