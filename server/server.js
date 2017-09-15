fbffbbfa8b778145d6b840ada3daafd9d2a8b093
James <jbkuczma@buffalo.edu>
2017-09-15 09:13:25 -0400

    for #5 add body-parser for json

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+
+app.use(bodyParser.json());
 
 app.get('/login', function(request, response) {
     response.send('login page');
