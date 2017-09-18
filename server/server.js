ee88d7ee23e3a2e21d60a5acfb59f45340e01660
James <jbkuczma@buffalo.edu>
2017-09-18 17:52:29 -0400

    for #5 created more post request routes

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-/*** GET ***/
+/*** GET REQUESTS ***/
 app.get('/login', function(request, response) {
     response.send('login page');
     // response.sendFile(path.join(__dirname + '/../www/html/login.html')); // uncoment when branches are merged
@@ -20,49 +20,49 @@ app.get('/room/:roomCode', function(request, response) {
     response.send('a room page');
 });
 
-/*** POST ***/
-app.post('/login', function(request, response) {
+/*** POST REQUESTS ***/
+app.post('/account/login', function(request, response) {
+    let username = request.body.username;
+    let password = request.body.password;
+    console.log('login')
+     /*
+        @TODO: implement once database schema is determined, log user in
+        - check if password provided matches hashed password in db
+    */
+
+});
+
+app.post('/account/create', function(request, response) {
     let username = request.body.username;
     let password = request.body.password;
     let confirmPassword = request.body.confirm_password;
-    // username and password provided, user is trying to login
-    if(username && password && !confirmPassword) {
-        console.log('login')
-    }
-    // username, password, confirm password provided, user is trying to create an account
-    else if(username && password && confirmPassword) {
-        console.log('create account')
-        /*
-            @TODO: implement once database schema is determined
-            sql = 'SELECT FROM {tableName} (username, password) WHERE username=?';
-            execute sql with username var and check if a row was returned
-            if a row was returned, the username exists -> tell user to choose a new username
-            else
-                check to make sure password === confirmPassword -> if not then tell user
-                create a row in the user table for this username and password. HASH the password before inserting into database
-                sql = 'INSERT INTO {tableName} (username, password) VALUES (username, SHA2(password, 256))
-        */
-    }
-    else {
-        // @TODO: let user know there was an error
-        console.log('error')
-    }
-    return
+
+    console.log('create account')
+    /*
+        @TODO: implement once database schema is determined
+        sql = 'SELECT FROM {tableName} (username, password) WHERE username=?';
+        execute sql with username var and check if a row was returned
+        if a row was returned, the username exists -> tell user to choose a new username
+        else
+            check to make sure password === confirmPassword -> if not then tell user
+            create a row in the user table for this username and password. HASH the password before inserting into database
+            sql = 'INSERT INTO {tableName} (username, password) VALUES (username, SHA2(password, 256))
+    */
 });
 
-app.post('/rooms', function(request, response) {
+app.post('/rooms/create', function(request, response) {
     let roomName = request.body.room_name;
-    let roomJoinCode = request.body.join_code;
+    // @TODO: create a row in the rooms table in db for new room -> then send user to room page
+});
 
-    
-    if(roomName) {
-        // user is trying to create a room
-        // @TODO: create a row in the rooms table in db for new room -> then send user to room page
-    }
-    else if(roomJoinCode) {
-        // user is trying to join an existing room
-        // @TODO: send user to that room
-    }
+app.post('/rooms/join', function(request, response) {
+    let roomJoinCode = request.body.join_code;
+    // @TODO: send user to that room
+});
+
+app.post('/room/:roomCode/search', function(request, response) {
+    let searchQuery = request.body.query;
+    // @TODO: send query to YouTube API, parse results, send results to frontend to show
 });
 
 app.listen(3000, function() {
