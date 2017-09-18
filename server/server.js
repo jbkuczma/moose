1284f3f92539e10b3a48929ca6c268fd39d2e5f1
James <jbkuczma@buffalo.edu>
2017-09-18 18:42:28 -0400

    for #5 added a few comments for what each route is for

--- server/server.js
+++ server/server.js
@@ -7,31 +7,39 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 /*** GET REQUESTS ***/
+
+// serve login page
 app.get('/login', function(request, response) {
     response.send('login page');
     // response.sendFile(path.join(__dirname + '/../www/html/login.html')); // uncoment when branches are merged
 });
 
+// serve page to create a new room or join an existing room
 app.get('/rooms', function(request, response) {
     response.send('create or join a room page');
 });
 
+// serve a specific room page
 app.get('/room/:roomCode', function(request, response) {
+    // @TODO: retrieve room info from db (room code, currently playing song, songs in queue, etc.)
     response.send('a room page');
 });
 
 /*** POST REQUESTS ***/
+
+// log a user in
 app.post('/account/login', function(request, response) {
     let username = request.body.username;
     let password = request.body.password;
     console.log('login')
-     /*
+    /*
         @TODO: implement once database schema is determined, log user in
         - check if password provided matches hashed password in db
     */
 
 });
 
+// create an account
 app.post('/account/create', function(request, response) {
     let username = request.body.username;
     let password = request.body.password;
@@ -50,21 +58,26 @@ app.post('/account/create', function(request, response) {
     */
 });
 
+// create a room
 app.post('/rooms/create', function(request, response) {
     let roomName = request.body.room_name;
     // @TODO: create a row in the rooms table in db for new room -> then send user to room page
 });
 
+// join an existing room via room code
 app.post('/rooms/join', function(request, response) {
     let roomJoinCode = request.body.join_code;
     // @TODO: send user to that room
 });
 
+// search YouTube api using query input
 app.post('/room/:roomCode/search', function(request, response) {
     let searchQuery = request.body.query;
     // @TODO: send query to YouTube API, parse results, send results to frontend to show
 });
 
+
+// starts Moose on port 3000
 app.listen(3000, function() {
     console.log('Moose started on port 3000. Go to localhost:3000/login');
 });
