0b3b62b5ba3ad74ae2fc054437b8ce9ecef848e3
James <jbkuczma@buffalo.edu>
2017-09-26 08:29:12 -0400

    for #14 adjusted action of form for creating account

--- www/js/login.js
+++ www/js/login.js
@@ -1,10 +1,11 @@
 function showCreateAccountForm() {
-    var confirmPasswordInput = document.getElementById('confirm_password_input')
-    var createAccountButton = document.getElementById('createAccountButton')
-    var submitButton = document.getElementById('login_form_button')
+    var confirmPasswordInput = document.getElementById('confirm_password_input');
+    var createAccountButton = document.getElementById('createAccountButton');
+    var submitButton = document.getElementById('login_form_button');
+    var submitForm = document.getElementsByClassName('login_form')[0];
 
-    confirmPasswordInput.style.display = 'block' // reveal input for confirm password
-    createAccountButton.style.display = 'none' // hide the create account button
-    submitButton.value = 'Create Account' // change text in login button
-    
+    confirmPasswordInput.style.display = 'block'; // reveal input for confirm password
+    createAccountButton.style.display = 'none'; // hide the create account button
+    submitButton.value = 'Create Account'; // change text in login button
+    submitForm.action = '/account/create'; // change action of form 
 }
