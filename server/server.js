ff41be7b7d8b8395b5dadd6522bfaad9b9bf8c5d
Rmiller1673 <rm233@buffalo.edu>
2017-09-30 15:19:27 -0400

    backend on create room, fixing bugs

--- server/server.js
+++ server/server.js
@@ -17,13 +17,6 @@ let connection = mysql.createConnection({
 
 connection.connect();
 
-// example query
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-  if (error) throw error;
-  console.log('The solution is: ', results[0].solution);
-});
-connection.end();
-
 
 /*** Express config ***/
 app.use(express.static('www'));
@@ -76,13 +69,13 @@ app.post('/account/create', auth.authenticate('create-account', {
 // create a room
 app.post('/rooms/create', function(request, response) {
     let roomName = request.body.room_name;
-    if (roomName.length() <= 15 || roomName === "SaturdaysAreForTheBoys"){
+    if ((roomName.length <= 15 && roomName.length > 2)|| roomName === "SaturdaysAreForTheBoys"){
         //generates random 5 digit code that cannot be shorter than 5 digits
         let random_numb = Math.floor(Math.random()*89999 + 10000);
-        let SQL = 'INSERT INTO ROOMS (room_name, room_code, created_at, room_owner_name) VALUES (?, ?, ?, ?)';
+        let SQL = 'INSERT INTO rooms(room_name, room_code, created_at, room_owner_name) VALUES (?, ?, ?, ?)';
         connection.query(SQL, [roomName, random_numb, new Date(), request.session.passport.user], function (error, results, fields) {
             if (error){throw error;}
-            app.get('/room/' + random_numb)
+            response.redirect('/room/' + random_numb);
         });
     }
     // @TODO: create a row in the rooms table in db for new room -> then send user to room page
